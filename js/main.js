0b4d96e46001efaa38bc03ac7bde594b7d894c61
BXS-Archipelago <bxs@tutanota.com>
2021-03-03 15:07:58 +0000

    updated to Tabular Data-Part 3 for API with DOM lessons

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ const barsFunction = () => {
 
 
 // create a connection for the YELP API
-/* const baseURL = "https://ci-swapi.herokuapp.com/api/"
+ const baseURL = "https://ci-swapi.herokuapp.com/api/"
 
 function getData(type, cb) {
 var xhr = new XMLHttpRequest();
@@ -27,43 +27,36 @@ xhr.onreadystatechange = function() {
    xhr.send();
 }
 
+function getTableHeaders(obj) {
+   var tableHeaders = [];
+
+   Object.keys(obj).forEach(function(key){
+      tableHeaders.push(`<td>${key}</td>`)
+   })
+   return `<tr>${tableHeaders}</td>`;
+}
+
+
 function writeToDocument(type) {
+   var tableRows = [];
     var el = document.getElementById("data");
     el.innerHTML = "";
 
    getData(type, function(data){
     data = data.results
+    var tableHeaders = getTableHeaders(data[0]);
     
     data.forEach(function(item) {
-         el.innerHTML += "<p>" + item.name + "</p>";
+        var dataRow = [];
+      
+        Object.keys(item).forEach(function(key){
+           var rowData = item[key].toString();
+           var truncatedData = rowData.substring(0,15)
+           dataRow.push(`<td>${truncatedData}</td>`)
+           })
+        tableRows.push(`<tr>${dataRow}</tr>`);
         })
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
     })
 }
- */
-const baseURL = "https://ci-swapi.herokuapp.com/api/";
-
-function getData(type, cb) {
-    var xhr = new XMLHttpRequest();
-
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            cb(JSON.parse(this.responseText));
-        }
-    };
-
-    xhr.open("GET", baseURL + type + "/");
-    xhr.send();
-}
-
-function writeToDocument(type) {
-    var el = document.getElementById("data");
-    el.innerHTML = "";
-
-    getData(type, function(data) {
-        data = data.results;
-
-        data.forEach(function(item) {
-            el.innerHTML += "<p>" + item.name + "</p>";
-        });
-    });
-}
+ 
